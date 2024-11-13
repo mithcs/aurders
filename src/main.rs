069816415e75fbedfad609d0dbb00f46a60145a8
Mitesh Soni <smiteshhc@gmail.com>
2024-11-13 22:35:26 +0530

    did some work on v2

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,8 @@
-mod args;
-mod final_step;
 mod pkgbuild;
-mod shared;
-mod srcinfo;
-mod utils;
 
-use final_step::{add_to_repo, commit_to_repo, execute_makepkg, setup_repo};
-use pkgbuild::generate_pkgbuild;
-use shared::get_information;
-use srcinfo::generate_srcinfo;
-use utils::{dead, input_bool};
-
-use shared::Information;
+use pkgbuild::pkgbuild;
 
 fn main() {
-    let info_result = get_information();
-    let pkginfo: Information;
-
-    match info_result {
-        Some(info) => pkginfo = info,
-        None => {
-            eprintln!("Failed to get information.");
-            dead();
-            return; // rust made me do this
-        }
-    };
-
-    generate_pkgbuild(&pkginfo);
-    generate_srcinfo(&pkginfo);
-
-    execute_makepkg();
-
-    setup_repo(&pkginfo.pkgname, &pkginfo.pkgver, &pkginfo.pkgrel, &pkginfo.arch);
-    add_to_repo(&pkginfo.pkgname);
-
-    let want_to_commit = input_bool("Do you want to commit changes in git repository?(y/n): ");
-
-    if want_to_commit {
-        commit_to_repo();
-    } else {
-        println!("\nGreat! Do it your way, manually. As you wish.");
-    }
+    println!("Hello World");
+    pkgbuild();
 }
--- src/pkgbuild.rs
+++ src/pkgbuild.rs
@@ -1,140 +1,374 @@
-//! pkgbuild module handles the generation of pkgbuild
-use crate::utils::dead;
-use crate::Information;
+use inquire::list_option::ListOption;
+use inquire::validator::Validation;
+use inquire::{required, min_length, MultiSelect, Select, Text};
 
-use std::fs::{self, File};
-use std::io::{self, BufRead, Write};
+/// This function generates the pkgbuild
+pub fn pkgbuild() {
+    let (maintainer_name, maintainer_email) = get_maintainer();
+    println!("Got {maintainer_name}, {maintainer_email}");
 
-/// generate_pkgbuild generates and returns the PKGBUILD
-pub fn generate_pkgbuild(pkginfo: &Information) {
-    let template = get_template();
-    let pkgbuild: String;
+    let pkgname = get_pkgname();
+    println!("Got {pkgname}");
 
-    let build_commands = get_build_commands();
-    let package_commands = get_package_commands();
+    let pkgver = get_pkgver();
+    println!("Got {pkgver}");
 
-    match template {
-        Ok(output) => {
-            println!("\nGot PKGBUILD template.");
-            pkgbuild = output
-                .replace("{maintainer_name}", &pkginfo.maintainer_name)
-                .replace("{maintainer_email}", &pkginfo.maintainer_email)
-                .replace("{pkgname}", &pkginfo.pkgname)
-                .replace("{pkgver}", &pkginfo.pkgver)
-                .replace("{pkgrel}", &pkginfo.pkgrel)
-                .replace("{pkgdesc}", &pkginfo.pkgdesc)
-                .replace("{arch}", &pkginfo.arch)
-                .replace("{url}", &pkginfo.url)
-                .replace("{license}", &pkginfo.license)
-                .replace("{depends}", &pkginfo.depends)
-                .replace("{makedepends}", &pkginfo.makedepends)
-                .replace("{source}", &pkginfo.source)
-                .replace("{sha256sums}", &pkginfo.sha256sums)
-                .replace("{build}", &build_commands)
-                .replace("{package}", &package_commands);
+    let pkgrel = get_pkgrel();
+    println!("Got {pkgrel}");
 
-            save_pkgbuild(&pkgbuild);
-        }
-        Err(e) => {
-            eprintln!("Failed to generate PKGBUILD from template: {}.", e);
-            dead();
-        }
-    };
-}
+    let epoch = get_epoch();
+    println!("Got {epoch}");
 
-/// get_template retrieves and returns the contents of templates/PKGBUILD
-fn get_template() -> std::io::Result<String> {
-    let contents_vec = match fs::read("templates/PKGBUILD") {
-        Ok(output) => output,
-        Err(e) => {
-            eprintln!("Failed to read templates/PKGBUILD: {}.", e);
-            dead();
-            return Err(e);
-        }
-    };
+    let pkgdesc = get_pkgdesc();
+    println!("Got {pkgdesc}");
 
-    let contents = match String::from_utf8(contents_vec) {
-        Ok(output) => output,
-        Err(e) => {
-            eprintln!("Failed to convert Vec<u8> to String: {}.", e);
-            dead();
-            "ERRROOORRR".to_string()
-        }
-    };
+    let arch = get_arch();
+    println!("Got {arch}");
 
-    Ok(contents)
-}
+    let url = get_url();
+    println!("Got {url}");
 
-/// save_pkgbuild is a helper function to save PKGBUILD to disk
-fn save_pkgbuild(pkgbuild: &String) {
-    // create_new because it creates new file in read-write mode; errror if the file exists
-    // and making sure that possibly existing PKGBUILD does not get overwritten
-    let file_result = File::create_new("aurders/PKGBUILD");
+    let license = get_license();
+    println!("Got {license}");
 
-    match file_result {
-        Ok(mut file) => match file.write_all(pkgbuild.as_bytes()) {
-            Ok(_) => println!("Saved PKGBUILD to disk successfully."),
-            Err(e) => {
-                eprintln!("Failed to write to PKGBUILD: {}.", e);
-                dead();
-            }
-        },
-        Err(e) => {
-            eprintln!("Failed to create new PKGBUILD: {}.", e);
-            dead();
-        }
+    let (sources, i) = get_source();
+    println!("Total {i}");
+    for source in sources {
+        println!("Got {source}");
     }
+
+    let sums = get_sums();
+    println!("Got {sums}");
+
+    let install = get_install();
+    println!("Got {install}");
+
+    let changelog = get_changelog();
+    println!("Got {changelog}");
+
+    let depends = get_depends();
+    println!("Got {depends}");
+
+    let makedepends = get_makedepends();
+    println!("Got {makedepends}");
+
+    let checkdepends = get_checkdepends();
+    println!("Got {checkdepends}");
+
+    let optdepends = get_optdepends();
+    println!("Got {optdepends}");
+
+    let conflicts = get_conflicts();
+    println!("Got {conflicts}");
+
+    let provides = get_provides();
+    println!("Got {provides}");
+
+    let replaces = get_replaces();
+    println!("Got {replaces}");
+
+    let backup = get_backup();
+    println!("Got {backup}");
+
+    let prepare = get_prepare();
+    println!("Got {prepare}");
+
+    let build = get_build();
+    println!("Got {build}");
+
+    let check = get_check();
+    println!("Got {check}");
+
+    let package = get_package();
+    println!("Got {package}");
 }
 
-/// get_build_commads gets the build commands from user and returns it
-fn get_build_commands() -> String {
-    let mut build = String::new();
-    let stdin = io::stdin();
+/// This function gets maintainer name and email
+fn get_maintainer() -> (String, String) {
+    let name = Text::new("Enter maintainer name")
+        .with_help_message("The name of the maintainer")
+        .with_validator(required!())
+        .prompt()
+        .unwrap();
 
-    println!("\nEnter commands to add in build(). [\"qq\" or EOF signal to quit]");
+    let email = Text::new("Enter maintainer email")
+        .with_help_message("The email of the maintainer")
+        .with_validator(required!())
+        .prompt()
+        .unwrap();
 
-    // lock the stdin and take multiline input correctly
-    for line in stdin.lock().lines() {
-        match line {
-            Ok(input) => {
-                if input.trim() == "qq" {
-                    break;
-                }
-                build.push_str(&input);
-                build.push_str("\n");
-            }
-            Err(e) => {
-                eprintln!("Error reading line: {}.", e);
+    return (name, email);
+}
+
+/// This function gets pkgname from user and returns it
+fn get_pkgname() -> String {
+    return Text::new("Enter pkgname")
+        .with_help_message("The name of the package")
+        .with_validator(required!())
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets pkgver from user and returns it
+fn get_pkgver() -> String {
+    return Text::new("Enter pkgver")
+        .with_help_message("The version of software as released from the author")
+        .with_validator(required!())
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets pkgrel from user and returns it
+fn get_pkgrel() -> String {
+    return Text::new("Enter pkgrel")
+        .with_default("1")
+        .with_help_message("The release number specific to the distribution")
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets epoch from user and returns it
+fn get_epoch() -> String {
+    return Text::new("Enter epoch")
+        .with_help_message("Used to force package to be seen as newer than previous versino")
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets pkgdesc from user and returns it
+fn get_pkgdesc() -> String {
+    return Text::new("Enter pkgdesc")
+        .with_validator(required!())
+        .with_help_message("Brief description of the package")
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets target arch from user and returns it
+fn get_arch() -> String {
+    let options = vec!["x86_64", "i686", "any", "Custom"];
+
+    let validator = |a: &[ListOption<&&str>]| {
+        if a.len() == 0 {
+            return Ok(Validation::Invalid("Select atleast 1 architecture".into()));
+        } else {
+            return Ok(Validation::Valid);
+        }
+    };
+
+    let architectures = MultiSelect::new("Select target architecture", options)
+        .with_validator(validator)
+        .with_vim_mode(true)
+        .with_help_message("Select target architecture of package")
+        .prompt()
+        .unwrap();
+
+    let mut arch = String::new();
+
+    for architecture in architectures {
+        match architecture {
+            "any" => {
+                arch = "'any'".to_string();
                 break;
             }
-        }
-    }
+            "Custom" => {
+                let customs = Text::new("Enter custom architecture")
+                    .with_validator(required!())
+                    .prompt()
+                    .unwrap();
 
-    build.trim().to_string()
-}
+                let custom_iter = customs.split(" ");
 
-/// get_package_commads gets the package commands from user and returns it
-fn get_package_commands() -> String {
-    let mut package = String::new();
-    let stdin = io::stdin();
-
-    println!("\nEnter commands to add in package(). [\"qq\" or EOF signal to quit]");
-
-    for line in stdin.lock().lines() {
-        match line {
-            Ok(input) => {
-                if input.trim() == "qq" {
-                    break;
+                for custom in custom_iter {
+                    arch = arch + "'" + custom + "' ";
                 }
-                package.push_str(&input);
-                package.push_str("\n");
-            }
-            Err(e) => {
-                eprintln!("Error reading line: {}.", e);
+
                 break;
             }
+            _ => arch = arch + "'" + architecture + "' ",
+        };
+    }
+
+    return arch.trim().to_string();
+}
+
+/// This function gets url from user and returns it
+fn get_url() -> String {
+    return Text::new("Enter url")
+        .with_help_message("Enter the url associated with the software")
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets sums from user and returns it
+fn get_sums() -> String {
+    return "hello from get_sums()".to_string();
+}
+
+/// This function gets install from user and returns it
+fn get_install() -> String {
+    return Text::new("Enter install")
+        .with_help_message("Special install script that is to be included in the package.")
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets source from user and returns it
+fn get_source() -> (Vec<String>, u8) {
+    let mut sources: Vec<String> = vec![];
+
+    let mut source: String;
+    let mut i: u8 = 0;
+
+    loop {
+        i += 1;
+        source = Text::new(format!("Enter source {i}").as_str())
+            .with_validator(if i > 1 { min_length!(0) } else { min_length!(2) })
+            .with_help_message("Must reside in same directory as the PKGBUILD, or be a URL that makepkg can use to download the file.\nPress enter again to exit.")
+            .prompt()
+            .unwrap();
+
+        if source == "" {
+            i -= 1;
+            break;
+        } else {
+            sources.push(source.clone());
         }
     }
 
-    package.trim().to_string()
+    return (sources, i);
+}
+
+/// This function gets changelog from user and returns it
+fn get_changelog() -> String {
+    return Text::new("Enter changelog")
+            .with_help_message("Changelog file that is to be included in the package. Should reside in same dir as PKGBUILD. No need to be included in source")
+            .prompt().unwrap();
+}
+
+/// This function gets license from user and returns it
+fn get_license() -> String {
+    let licenses = vec![
+        "MIT",
+        "GPL-3.0",
+        "GPL-2.0",
+        "unknown",
+        "BSD-3",
+        "BSD-2",
+        "MPL-2.0",
+        "CC0",
+        "Apache-2.0",
+        "LGPL-3.0",
+        "AGPL-3.0",
+        "Custom",
+    ];
+
+    let license = Select::new("Select license", licenses)
+        .with_vim_mode(true)
+        .with_help_message("Select license of package")
+        .prompt()
+        .unwrap();
+
+    match license {
+        "Custom" => {
+            return Text::new("Enter custom architecture")
+                .with_validator(required!())
+                .prompt()
+                .unwrap();
+        }
+        _ => return license.to_string(),
+    };
+}
+
+// should do?; groups
+
+/// This function gets depends from user and returns it
+fn get_depends() -> String {
+    return Text::new("Enter dependencies of this package")
+        .with_help_message("Packages this package depends on to run")
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets make depends from user and returns it
+fn get_makedepends() -> String {
+    return Text::new("Enter make dependencies of this package")
+        .with_help_message(
+            "Packages this package depends on to build but are not needed at runtime",
+        )
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets check depends from user and returns it
+fn get_checkdepends() -> String {
+    return Text::new("Enter check dependencies of this package")
+        .with_help_message(
+            "Packages this package depends on to run its test suite but are not needed at runtime",
+        )
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets opt depends from user and returns it
+fn get_optdepends() -> String {
+    return Text::new("Enter opt dependencies of this package")
+            .with_help_message("Packages that are not essential for base functionality, but may be necessary to make full use of the contents of this package")
+            .prompt().unwrap();
+}
+
+/// This function gets conflics from user and returns it
+fn get_conflicts() -> String {
+    return Text::new("Enter conflicts")
+        .with_help_message("Packages that will conflict with this package")
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets provides from user and returns it
+fn get_provides() -> String {
+    return Text::new("Enter provides")
+        .with_help_message("Virtual provisions/packages that this package provides")
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets provides from user and returns it
+fn get_replaces() -> String {
+    return Text::new("Enter replaces")
+        .with_help_message(
+            "Packages that this package should replace. Used to handle renamed/combined packages",
+        )
+        .prompt()
+        .unwrap();
+}
+
+// should do?; options
+
+/// This function gets backup from user and returns it
+fn get_backup() -> String {
+    return Text::new("Enter backup")
+        .with_help_message("Files that should be backed up if the package is removed or upgraded.")
+        .prompt()
+        .unwrap();
+}
+
+/// This function gets prepare function from user and returns it
+fn get_prepare() -> String {
+    return "Hello from get_prepare()".to_string();
+}
+
+/// This function gets build function from user and returns it
+fn get_build() -> String {
+    return "Hello from get_build()".to_string();
+}
+
+/// This function gets check function from user and returns it
+fn get_check() -> String {
+    return "Hello from get_check()".to_string();
+}
+
+/// This function gets package function from user and returns it
+fn get_package() -> String {
+    return "Hello from get_package()".to_string();
 }
